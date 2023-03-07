6e592d61d65fb10e248682da162560e566cd3977
UdeA-rediensE <esneider.ballesta@udea.edu.co>
2023-03-06 20:15:22 -0500

    Part 4. Add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes = HEROES;
+
+export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     if (this.selectedHero === hero) {
       this.selectedHero = undefined; // Si se hace clic en el héroe seleccionado, se quita la selección.
     } else {
       this.selectedHero = hero; // Si se hace clic en otro héroe, se actualiza la selección.
+      this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
     }
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
